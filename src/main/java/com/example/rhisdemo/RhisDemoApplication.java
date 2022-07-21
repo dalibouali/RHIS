8c92aaa7abae977df18471bddb499c9f1308d5b9
houssemeddin07 <houssemeddin.loudhabachi@esprit.tn>
2022-07-21 16:37:21 +0100

    second commit by houssem

--- src/main/java/com/example/rhisdemo/RhisDemoApplication.java
+++ src/main/java/com/example/rhisdemo/RhisDemoApplication.java
@@ -2,8 +2,12 @@ package com.example.rhisdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
+@EnableAspectJAutoProxy
 public class RhisDemoApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/rhisdemo/controller/usercontroller.java
+++ src/main/java/com/example/rhisdemo/controller/usercontroller.java
@@ -15,6 +15,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
+@CrossOrigin(origins ="*")
 public class usercontroller {
 
 
